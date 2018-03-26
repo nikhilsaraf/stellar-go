564c65a2f7973392a135423e8631a236de182cc0
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2018-03-26 14:04:08 -0700

    stellar_create_asset: support for passing secret key using a file and stdin

--- assets/stellar_create_asset/stellar_create_asset.go
+++ assets/stellar_create_asset/stellar_create_asset.go
@@ -4,6 +4,10 @@ import (
     "fmt"
     "flag"
     "log"
+    "bufio"
+    "io/ioutil"
+    "os"
+    "strings"
     "github.com/stellar/go/keypair"
     "github.com/stellar/go/clients/horizon"
     b "github.com/stellar/go/build"
@@ -20,24 +24,43 @@ func getAddress(seed string) string {
 func main() {
     codePtr := flag.String("code", "", "the code for the asset")
     issuerAddressPtr := flag.String("issuer", "", "the issuer's address")
-    receiverSeedPtr := flag.String("receiverSeed", "", "the receiver's seed")
+    receiverSeedPtr := flag.String("receiverSeed", "", "(optional) the receiver's seed. will read from standard in if this as well as seedFile is unspecified. this takes precedence over the seedFile arg")
+    seedFilePtr := flag.String("seedFile", "", "(optional) path to file with the receiver's seed. will read from standard in if this as well as receiverSeed is unspecified. file should contain only the secret and nothing else, no extra spaces.")
     networkPtr := flag.String("network", "", "t for testnet, p for pubnet")
     limitPtr := flag.Int("limit", 0, "(optional) limit for trust, 0 for max limit")
     flag.Parse()
 
-    if *codePtr == "" || *issuerAddressPtr == "" || *receiverSeedPtr == "" || (*networkPtr != "t" && *networkPtr != "p") {
+    if *codePtr == "" || *issuerAddressPtr == "" || (*networkPtr != "t" && *networkPtr != "p") {
         flag.PrintDefaults()
         return
     }
+
+    var receiverSeed string
+    if *receiverSeedPtr != "" {
+        receiverSeed = *receiverSeedPtr
+    } else if *seedFilePtr != "" {
+        dat, err := ioutil.ReadFile(*seedFilePtr)
+        if err != nil {
+            fmt.Println("error in file: ", err)
+            flag.PrintDefaults()
+            return
+        }
+        receiverSeed = string(dat)
+    } else {
+        fmt.Println("enter secret seed from which to create the trust line:")
+        reader := bufio.NewReader(os.Stdin)
+        secret, _ := reader.ReadString('\n')
+        receiverSeed = strings.Replace(secret, "\n", "", -1)
+    }    
+
     code := *codePtr
     issuerAddress := *issuerAddressPtr
-    receiverSeed := *receiverSeedPtr
     receiverAddress := getAddress(receiverSeed)
     network := *networkPtr
     limit := *limitPtr
     fmt.Println("code:", code)
     fmt.Println("issuerAddress:", issuerAddress)
-    fmt.Println("receiverSeed:", receiverSeed)
+    //fmt.Println("receiverSeed:", receiverSeed)
     fmt.Println("receiverAddress:", receiverAddress)
     fmt.Println("network:", network)
     fmt.Println("limit:", limit)
